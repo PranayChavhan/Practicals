f14a4b63ca53a13bdb840f8280bee14aa66ca107
PranayChavhan <pranay21chavhan@outlook.com>
2023-05-24 17:08:21 +0530

    Prim's algorithm

--- Practical_07.cpp
+++ Practical_07.cpp
@@ -1,86 +1,90 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <limits.h> // Include the limits header for INT_MAX
+#include <limits>
 
 using namespace std;
 
-// Structure to represent an edge in the graph
-struct Edge {
-    int src, dest, weight;
-};
+const int MAX_OFFICES = 10; // Maximum number of offices
 
-// Structure to represent a graph
-struct Graph {
-    int V; // Number of vertices
-    int E; // Number of edges
+// Class to represent a graph
+class Graph {
+private:
+    int numOffices;
+    int adjacencyMatrix[MAX_OFFICES][MAX_OFFICES];
 
-    vector<Edge> edges; // Vector to store edges
+public:
+    Graph(int offices) {
+        numOffices = offices;
 
-    Graph(int v, int e) {
-        V = v;
-        E = e;
-    }
-};
-
-// Function to add an edge to the graph
-void addEdge(Graph& graph, int src, int dest, int weight) {
-    Edge edge;
-    edge.src = src;
-    edge.dest = dest;
-    edge.weight = weight;
-    graph.edges.push_back(edge);
-}
-
-// Function to find the minimum cost spanning tree using Prim's algorithm
-int primMST(Graph& graph) {
-    // Priority queue to store vertices and their key values
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    int src = 0; // Start from the first vertex
-
-    // Create a vector for keys and initialize all key values as infinite
-    vector<int> key(graph.V, INT_MAX);
-
-    // Create a vector to store the parent of each vertex in the MST
-    vector<int> parent(graph.V, -1);
-
-    // Create a vector to track whether a vertex is included in MST or not
-    vector<bool> inMST(graph.V, false);
-
-    // Insert the source vertex into the priority queue and initialize its key as 0
-    pq.push(make_pair(0, src));
-    key[src] = 0;
-
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-
-        inMST[u] = true;
-
-        // Iterate through all adjacent vertices of u
-        for (auto& edge : graph.edges) {
-            int v = (edge.src == u) ? edge.dest : edge.src;
-            int weight = edge.weight;
-
-            // If v is not yet included in MST and weight of (u,v) is smaller than the current key of v
-            if (inMST[v] == false && weight < key[v]) {
-                // Update the key value of v
-                key[v] = weight;
-                pq.push(make_pair(key[v], v));
-                parent[v] = u;
+        // Initialize the adjacency matrix with 0 (no edge)
+        for (int i = 0; i < numOffices; ++i) {
+            for (int j = 0; j < numOffices; ++j) {
+                adjacencyMatrix[i][j] = 0;
             }
         }
     }
 
-    // Calculate the total cost by summing up the key values of all vertices (excluding the source vertex)
-    int totalCost = 0;
-    for (int i = 1; i < graph.V; i++) {
-        totalCost += key[i];
+    void addEdge(int src, int dest, int cost) {
+        // Since the graph is undirected, set both src->dest and dest->src with the same cost
+        adjacencyMatrix[src][dest] = cost;
+        adjacencyMatrix[dest][src] = cost;
     }
 
-    return totalCost;
-}
+    void displayMatrix() {
+        cout << "Adjacency Matrix:" << endl;
+        for (int i = 0; i < numOffices; ++i) {
+            for (int j = 0; j < numOffices; ++j) {
+                cout << adjacencyMatrix[i][j] << "\t";
+            }
+            cout << endl;
+        }
+    }
+
+    int primMST() {
+        int key[MAX_OFFICES];
+        bool inMST[MAX_OFFICES];
+        int parent[MAX_OFFICES];
+
+        // Initialize key values and MST sets
+        for (int i = 0; i < numOffices; ++i) {
+            key[i] = numeric_limits<int>::max();
+            inMST[i] = false;
+        }
+
+        key[0] = 0; // Start with the first vertex
+
+        int totalCost = 0;
+
+        // Find the minimum cost spanning tree
+        for (int count = 0; count < numOffices - 1; ++count) {
+            int minKey = numeric_limits<int>::max();
+            int minIndex;
+
+            // Find the vertex with the minimum key value that is not yet included in MST
+            for (int v = 0; v < numOffices; ++v) {
+                if (!inMST[v] && key[v] < minKey) {
+                    minKey = key[v];
+                    minIndex = v;
+                }
+            }
+
+            int u = minIndex;
+            inMST[u] = true;
+
+            // Add the cost of the selected edge to the total cost
+            totalCost += key[u];
+
+            // Update the key values of adjacent vertices
+            for (int v = 0; v < numOffices; ++v) {
+                if (adjacencyMatrix[u][v] && !inMST[v] && adjacencyMatrix[u][v] < key[v]) {
+                    key[v] = adjacencyMatrix[u][v];
+                    parent[v] = u;
+                }
+            }
+        }
+
+        return totalCost;
+    }
+};
 
 int main() {
     int numOffices, numConnections;
@@ -90,16 +94,18 @@ int main() {
     cout << "Enter the number of connections: ";
     cin >> numConnections;
 
-    Graph graph(numOffices, numConnections);
+    Graph graph(numOffices);
 
-    for (int i = 0; i < numConnections; i++) {
-        int src, dest, weight;
-        cout << "Enter connection " << (i + 1) << " (source destination weight): ";
-        cin >> src >> dest >> weight;
-        addEdge(graph, src, dest, weight);
+    for (int i = 0; i < numConnections; ++i) {
+        int src, dest, cost;
+        cout << "Enter connection " << (i + 1) << " (source destination cost): ";
+        cin >> src >> dest >> cost;
+        graph.addEdge(src, dest, cost);
     }
 
-    int minCost = primMST(graph);
+    graph.displayMatrix();
+
+    int minCost = graph.primMST();
     cout << "Minimum cost to connect all offices: " << minCost << endl;
 
     return 0;
